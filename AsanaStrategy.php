1fc54007ee7a68d26e4ddc25dd0ee105f85c5649
David Wambugu <davidwambugu@gmail.com>
2013-05-08 14:51:35 +0300

    initialized the repository

---
+++ AsanaStrategy.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of AsanaStrategy
+ *
+ * @author David Wambugu <davidwambugu@gmail.com>
+ */
+class AsanaStrategy extends OpauthStrategy{
+    
+    
+ 
+}
